56d29416a86f90d107649c6674d10219a542cdb6
Caleb Evans <caleb@calebevans.me>
2019-02-10 15:03:35 -0800

    Prepare v1.0.0+post.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_long_description():
 
 setup(
     name='bump-anything',
-    version='1.0.0',
+    version='1.0.0.post1',
     description='A CLI utility for bumping the version of any file type',
     long_description=get_long_description(),
     url='https://github.com/caleb531/bump-anything',
