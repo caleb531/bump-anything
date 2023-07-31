6f845f794038968813a793a552677fec22bcc4ef
Caleb Evans <caleb@calebevans.me>
2023-07-30 18:50:23 -0700

    Add test for file with no version found

--- tests/test_edge.py
+++ tests/test_edge.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from nose2.tools.decorators import with_setup, with_teardown
 
 import bump_anything.__main__ as bump
-from tests import set_up, tear_down
+from tests import create_mock_file, set_up, tear_down
 from tests.decorators import redirect_stderr, redirect_stdout
 
 case = unittest.TestCase()
@@ -28,5 +28,20 @@ def test_nonexistent_file_explicit(out, err):
 @patch("sys.argv", [bump.__file__, "major"])
 @redirect_stdout
 def test_nonexistent_file_implicit(out):
+    bump.main()
+    case.assertIn("No files updated", out.getvalue())
+
+
+@with_setup(set_up)
+@with_teardown(tear_down)
+@patch("sys.argv", [bump.__file__, "major", "foo.py"])
+@redirect_stdout
+def test_no_version_found(out):
+    create_mock_file(
+        "foo.py",
+        """
+        There's no version here.. unless you brought it with you!
+    """,
+    )
     bump.main()
     case.assertIn("No files updated", out.getvalue())
